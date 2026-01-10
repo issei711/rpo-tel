3b029100b6494887cca3432bfeb38a53245d1723
issei711 <i_s3.10@icloud.com>
2026-01-10 15:36:20 +0900

    entrypoint.shがエラーログを出力するように設定

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,15 @@ set -eu
 
 echo "entrypoint start"
 echo "PORT=${PORT}"
+echo "DJANGO_SETTINGS_MODULE=${DJANGO_SETTINGS_MODULE:-}"
+echo "PYTHONUNBUFFERED=${PYTHONUNBUFFERED:-}"
 
 exec gunicorn config.wsgi:application \
   --bind 0.0.0.0:${PORT} \
   --workers 1 \
   --threads 2 \
-  --timeout 0
+  --timeout 0 \
+  --access-logfile - \
+  --error-logfile - \
+  --capture-output \
+  --log-level debug
